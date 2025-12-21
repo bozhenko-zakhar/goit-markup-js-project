58f00352e221143d364f8d406a49ca25ccc1308c
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2025-12-22 00:36:36 +0100

    faster Fix to footer icon styles

--- src/js/untils/render-footer-svges.js
+++ src/js/untils/render-footer-svges.js
@@ -1,7 +1,7 @@
 import sprite from "../../img/sprite.svg";
 
 const logoContainer = document.querySelector(".footer-logo");
-const footerIcons = document.querySelectorAll(".footer-social-links-list-item");
+const footerIcons = document.querySelector(".footer-social-links-list");
 
 const logo = `
 <svg class="footer-logo-icon">
@@ -9,20 +9,24 @@ const logo = `
 </svg>
 `
 
-const svg1 = `
-<svg class="footer-social-icon-unit">
-		<use href="${sprite}#icon-facebook"></use>
-</svg>
-`
-
-const svg2 = `
-<svg class="footer-social-icon-unit">
-		<use href="${sprite}#icon-instagram"></use>
-</svg>
+const svgItems = `
+<li class="footer-social-links-list-item">
+	<a href="https://www.facebook.com/" class="footer-social-icon" target="_blank">
+		<svg class="footer-social-icon-unit">
+				<use href="${sprite}#icon-facebook"></use>
+		</svg>
+	</a>
+</li>
+<li class="footer-social-links-list-item">
+	<a href="https://www.instagram.com/" class="footer-social-icon" target="_blank">
+		<svg class="footer-social-icon-unit">
+				<use href="${sprite}#icon-instagram"></use>
+		</svg>
+	</a>
+</li>
 `
 
 export default function renderFooterSvges() {
 	logoContainer.innerHTML = logo;
-	footerIcons[0].innerHTML = svg1;
-	footerIcons[1].innerHTML = svg2;
+	footerIcons.innerHTML = svgItems;
 }
